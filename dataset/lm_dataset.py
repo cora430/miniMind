0cab57339eb3ece9e3712d622a0f76d01024dbc5
cora430 <9753065+cora430@user.noreply.gitee.com>
2026-03-18 18:11:47 +0800

    添加了sft的部分

--- dataset/lm_dataset.py
+++ dataset/lm_dataset.py
@@ -7,6 +7,27 @@ from datasets import load_dataset
 from transformers.activations import FastGELUActivation
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
+def pre_processing_chat(conversations, add_system_ratio=0.2):
+    SYSTEM_PROMPTS = [
+        "你是一个知识丰富的AI，尽力为用户提供准确的信息。",
+        "你是minimind，一个小巧但有用的语言模型。",
+        "你是一个专业的AI助手，请提供有价值的回答。",
+        "你是minimind，请尽力帮助用户解决问题。",
+        "你是一个可靠的AI，请给出准确的回答。",
+        "You are a helpful AI assistant.",
+        "You are minimind, a lightweight intelligent assistant.",
+        "You are a friendly chatbot. Please answer the user's questions carefully.",
+        "You are a knowledgeable AI. Try your best to provide accurate information.",
+        "You are minimind, a small but useful language model."
+    ]
+    if conversations and conversations[0]["role"] != "system":
+        if random.random() < add_system_ratio:
+            return [{"role": "system", "content": random.choice(SYSTEM_PROMPTS)}] + conversations    
+    return conversations
+def post_processing_chat(prompt_content, empty_think_ratio=0.05):
+    if '<think>\n\n</think>\n\n' in prompt_content and random.random() > empty_think_ratio:
+        prompt_content = prompt_content.replace('<think>\n\n</think>\n\n', "")
+    return prompt_content
 class PretrainDataset(Dataset):
     def __init__(self, datapath, tokenizer, max_length=512):
         super().__init__()
@@ -25,3 +46,55 @@ class PretrainDataset(Dataset):
         labels = input_ids.clone()
         labels[input_ids == self.tokenizer.pad_token_id] = -100
         return input_ids, labels
+
+class SFTDataset(Dataset):
+    def __init__(self, jsonl_path, tokenizer, max_length=1024):
+        super().__init__()
+        self.samples = load_dataset("json", data_files=jsonl_path, split="train")
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+        self.bos_id = tokenizer(f"{tokenizer.bos_token}assistant\n", add_special_tokens=False).input_ids
+        self.eos_id = tokenizer(f"{tokenizer.eos_token}\n", add_special_tokens=False).input_ids
+    def __len__(self):
+        return len(self.samples)
+    def create_chat_prompt(self, conversions):
+        tools = conversions[0].get("functions", None) if (conversions and conversions[0]["role"] == "system") else None
+        return self.tokenizer.apply_chat_template(conversions, tokenize=False, add_generation_prompt=False, tools=tools)
+    def generate_labels(self, input_ids):
+        labels = [-100] * len(input_ids)
+        i = 0
+        while i < len(input_ids):
+            if input_ids[i:i+len(self.bos_id)] == self.bos_id:
+                start = i + len(self.bos_id)
+                end = start
+                while end < len(input_ids):
+                    if input_ids[end:end + len(self.eos_id)] == self.eos_id:
+                        break
+                    end += 1
+                for j in range(start, min(end+len(self.eos_id), len(input_ids))):
+                    labels[j] = input_ids[j]
+                i = min(end+len(self.eos_id), len(input_ids))
+            else:
+                i += 1
+
+        return labels
+    def __getitem__(self, index):
+        conversations = self.samples[index]["conversations"]
+        conversations = pre_processing_chat(conversations)
+        prompt = self.create_chat_prompt(conversations)
+        prompt = post_processing_chat(prompt)
+        input_ids = self.tokenizer(prompt).input_ids[:self.max_length]
+        input_ids = input_ids + [self.tokenizer.pad_token_id] * (self.max_length - len(input_ids))
+        labels = self.generate_labels(input_ids)
+        for i in range(len(input_ids)):
+            if input_ids[i] == self.tokenizer.pad_token_id:
+                labels[i] = -100
+        return torch.tensor(input_ids, dtype=torch.long), torch.tensor(labels, dtype=torch.long)
+    
+
+    
+
+
+
+
+        
---
+++ trainer/train_full_sft.py
@@ -0,0 +1,167 @@
+from contextlib import nullcontext
+import sys
+import os 
+import argparse
+import time
+from h11 import Data
+from sentry_sdk import is_initialized
+import torch
+import warnings
+
+warnings.filterwarnings("ignore")
+__package__ = "trainer"
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from trainer.trainer_utils import get_lr, init_model, Logger, is_main_process,lm_checkpoint,init_distributed_mode, setup_seed, SkipBatchSampler
+from model.model_minimind import MiniMindConfig
+from dataset.lm_dataset import SFTDataset
+import torch.distributed as dist
+from torch.utils.data import DistributedSampler, DataLoader
+from torch.nn.parallel import DistributedDataParallel
+
+def train_epoch(epoch, loader, iters, start_step=0, wandb=None):
+    model.train()
+    start_time = time.time()
+    steps_done_this_run = 0
+    for step, (input_ids, labels) in enumerate(loader, start_step):
+        steps_done_this_run += 1
+        input_ids = input_ids.to(args.device)
+        labels = labels.to(args.device)
+        lr = get_lr(step+1+epoch*iters, args.epochs*iters, lr=args.learning_rate)
+        for pg in optimizer.param_groups:
+            pg["lr"] = lr
+        with autocast_ctx:
+            res = model(input_ids=input_ids, labels=labels)
+            loss = res.loss + res.aux_loss
+            loss = loss / args.accumulation_steps
+        scaler.scale(loss).backward()
+        if (step+1) % args.accumulation_steps == 0:
+            scaler.unscale_(optimizer)
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=args.grad_clip)
+            scaler.step(optimizer)
+            scaler.update()
+            optimizer.zero_grad(set_to_none=True)
+        if (step+1) % args.log_interval == 0 or step == iters - 1:
+            spend_time = time.time() - start_time
+            eta_min = spend_time / steps_done_this_run * (iters - step - 1) // 60
+            current_loss = loss.item() * args.accumulation_steps
+            current_logits_loss = res.loss.item()
+            current_aux_loss = res.aux_loss.item()
+            Logger(
+                f"Epoch:[{epoch + 1}/{args.epochs}]({step + 1}/{iters}), "
+                f"loss: {current_loss:.4f}, "
+                f"logits loss:{current_logits_loss:.4f}, "
+                f"aux loss:{current_aux_loss:.4f}, "
+                f"lr:{lr:.8f}, "
+                f"eta min:{eta_min:.1f}"
+            )
+            if wandb:
+                wandb.log({
+                    "loss":current_loss,
+                    "logits_loss":current_logits_loss,
+                    "aux_loss":current_aux_loss,
+                    "learning_rate":lr,
+                    "epoch_time":eta_min
+                })
+        if ((step+1) % args.save_interval == 0 or step == iters - 1 ) and is_main_process():
+            model.eval()
+            lm_checkpoint(lm_config, args.save_weight, model, optimizer, epoch, step, wandb, '/root/autodl-tmp/miniMind/checkpoints', scaler=scaler)
+            model.train()
+        del input_ids, labels, res, loss
+
+
+
+
+    
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser(description="MiniMind Full SFT")
+    parser.add_argument("--save_dir", type=str, default="/root/autodl-tmp/miniMind/out", help="模型保存目录")
+    parser.add_argument('--save_weight', default='full_sft', type=str, help="保存权重的前缀名")
+    parser.add_argument("--epochs", type=int, default=2, help="训练轮数")
+    parser.add_argument("--batch_size", type=int, default=16, help="batch size")
+    parser.add_argument("--learning_rate", type=float, default=1e-6, help="初始学习率")
+    parser.add_argument("--device", type=str, default="cuda:0" if torch.cuda.is_available() else "cpu", help="训练设备")
+    parser.add_argument("--dtype", type=str, default="bfloat16", help="混合精度类型")
+    parser.add_argument("--num_workers", type=int, default=8, help="数据加载线程数")
+    parser.add_argument("--accumulation_steps", type=int, default=1, help="梯度累积步数")
+    parser.add_argument("--grad_clip", type=float, default=1.0, help="梯度裁剪阈值")
+    parser.add_argument("--log_interval", type=int, default=100, help="日志打印间隔")
+    parser.add_argument("--save_interval", type=int, default=1000, help="模型保存间隔")
+    parser.add_argument('--hidden_size', default=512, type=int, help="隐藏层维度")
+    parser.add_argument('--num_hidden_layers', default=8, type=int, help="隐藏层数量")
+    parser.add_argument('--max_seq_len', default=340, type=int, help="训练的最大截断长度（中文1token≈1.5~1.7字符）")
+    parser.add_argument('--use_moe', default=0, type=int, choices=[0, 1], help="是否使用MoE架构（0=否，1=是）")
+    parser.add_argument("--data_path", type=str, default="/root/autodl-tmp/miniMind/dataset/sft_mini_512.jsonl", help="训练数据路径")
+    parser.add_argument('--from_weight', default='pretrain', type=str, help="基于哪个权重训练，为none则不基于任何权重训练")
+    parser.add_argument('--from_resume', default=0, type=int, choices=[0, 1], help="是否自动检测&续训（0=否，1=是）")
+    parser.add_argument("--use_wandb", action="store_true", help="是否使用wandb")
+    parser.add_argument("--wandb_project", type=str, default="MiniMind-Full-SFT", help="wandb项目名")
+    parser.add_argument("--use_compile", default=0, type=int, choices=[0, 1], help="是否使用torch.compile加速（0=否，1=是）")
+    args = parser.parse_args()
+    # ========== 1. 初始化环境和随机种子 ==========
+    local_rank = init_distributed_mode()
+    if dist.is_initialized(): args.device = f"cuda:{local_rank}"
+    setup_seed(42 + dist.get_rank() if dist.is_initialized() else 0)
+    # ========== 2. 配置目录、模型参数、检查ckp ==========
+    os.makedirs(args.save_dir, exist_ok=True)
+    lm_config = MiniMindConfig(
+        hidden_size=args.hidden_size,
+        use_moe=bool(args.use_moe),
+        num_hidden_layers=args.num_hidden_layers
+    )
+    ckp_data = lm_checkpoint(lm_config, weight=args.save_weight, save_dir='/root/autodl-tmp/miniMind/checkpoints') if args.from_resume==1 else None
+    # ========== 3. 设置混合精度 ==========
+    dtype = torch.bfloat16 if args.dtype == 'bfloat16' else torch.float16
+    device_type =  "cuda" if "cuda" in args.device else "cpu"
+    autocast_ctx = torch.amp.autocast(device_type=device_type, dtype=dtype, enabled=(device_type=="cuda"))
+    # ========== 4. 配wandb ==========
+    wandb = None
+    if args.use_wandb:
+        import swanlab as wandb
+        wandb_id = ckp_data["wandb_id"] if ckp_data is not None else None
+        run_name = f"MiniMind Pretrained epoches : {args.epochs} - batch_size : {args.batch_size} - learning_rate : {args.learning_rate} - use_moe: {args.use_moe}"
+        resume = "must" if args.from_resume == 1 else None
+        wandb.init(project=args.wandb_project, experiment_name=run_name, id=wandb_id, resume=resume, config=lm_config.to_dict())
+    # ========== 5. 定义模型、数据、优化器 ==========
+    model, tokenizer = init_model(lm_config, args.from_weight, device=args.device)
+    if args.use_compile ==1:
+        model = torch.compile(model)
+        Logger("torch.compile enabled")
+    train_ds = SFTDataset(args.data_path, tokenizer, args.max_seq_len)
+    sampler = DistributedSampler(train_ds) if dist.is_initialized() else None
+    scaler = torch.amp.GradScaler(device=device_type, enabled=(args.dtype == 'float16'))
+    optimizer = torch.optim.AdamW(model.parameters(), lr=args.learning_rate)
+    # ========== 6. 从ckp恢复状态 ==========
+    start_epoch, start_step = 0, 0
+    if ckp_data is not None:
+        model.load_state_dict(ckp_data["model"])
+        optimizer.load_state_dict(ckp_data["optimizer"])
+        start_epoch = ckp_data["epoch"]
+        start_step = ckp_data["step"]
+        if "scaler" in ckp_data and scaler is not None: scaler.load_state_dict(ckp_data["scaler"])  
+        Logger(f"成功从 checkpoint 恢复: Epoch {start_epoch}, Step {start_step}")
+    del ckp_data
+    # ========== 7. DDP包模型 ==========
+    if dist.is_initialized():
+        model._ddp_params_and_buffers_to_ignore = {"freqs_cos", "freqs_sin"}
+        model = DistributedDataParallel(model, device_ids=[local_rank])
+    
+    # ========== 8. 开始训练 ==========
+    for epoch in range(start_epoch,args.epochs):
+        sampler and sampler.set_epoch(epoch)
+        setup_seed(42 + epoch)
+        indices = torch.randperm(len(train_ds)).tolist()
+        skip = start_step if (epoch == start_epoch and start_step > 0) else 0
+        sampler = SkipBatchSampler(sampler or indices, args.batch_size, skip_batches=skip)
+        loader = DataLoader(train_ds, sampler=sampler, num_workers=args.num_workers, pin_memory=True)
+        Logger(f"{epoch + 1}/{args.epochs}  跳过前{skip}个batch, 从{skip}开始")
+        train_epoch(epoch, loader, len(loader)+skip, skip, wandb=wandb)
+    if dist.is_initialized(): dist.destroy_process_group()
+
+
+
+
+    
+    
+
